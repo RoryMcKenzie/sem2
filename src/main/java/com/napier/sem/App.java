7defd8ee951c0d3642b93586a2209fae42488132
RoryMcKenzie <rorymckenzie@mailbox.org>
2020-03-03 18:14:48 +0000

    Added Salary by Role

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -18,6 +18,10 @@ public class App
 
         a.printSalaries(employees);
 
+        ArrayList<Employee> employeesByRole = a.getAllSalariesByRole("Senior Engineer");
+        a.printSalaries(employeesByRole);
+
+
         // Disconnect from database
         a.disconnect();
     }
@@ -177,6 +181,7 @@ public class App
         }
     }
 
+
     /**
      * Prints a list of employees.
      * @param employees The list of employees to print.
@@ -195,4 +200,56 @@ public class App
         }
     }
 
+    public ArrayList<Employee> getAllSalariesByRole(String role)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect =
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary\n" +
+                            "FROM employees, salaries, titles\n" +
+                            "WHERE employees.emp_no = salaries.emp_no\n" +
+                            "AND employees.emp_no = titles.emp_no\n" +
+                            "AND salaries.to_date = '9999-01-01'\n" +
+                            "AND titles.to_date = '9999-01-01'\n" +
+                            "AND titles.title = '" + role + "' \n" +
+                            "ORDER BY employees.emp_no ASC";
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<Employee> employees = new ArrayList<Employee>();
+            while (rset.next())
+            {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("employees.emp_no");
+                emp.first_name = rset.getString("employees.first_name");
+                emp.last_name = rset.getString("employees.last_name");
+                emp.salary = rset.getInt("salaries.salary");
+                employees.add(emp);
+            }
+            return employees;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get salary details by role");
+            return null;
+        }
+    }
+
 }
+
+
+/*
+
+SELECT employees.emp_no, employees.first_name, employees.last_name, salaries.salary
+FROM employees, salaries, titles
+WHERE employees.emp_no = salaries.emp_no
+AND employees.emp_no = titles.emp_no
+AND salaries.to_date = '9999-01-01'
+AND titles.to_date = '9999-01-01'
+AND titles.title = 'Senior Engineer'
+ORDER BY employees.emp_no ASC;
+ */
